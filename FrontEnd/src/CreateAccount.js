728c9ff7db1733e667c9440b1ed874cc4a31fd0e
hp6164 <hp6164@hotmail.com>
2023-12-08 19:01:31 -0800

    Login/CreateAcc now submits and redirects

--- FrontEnd/src/CreateAccount.js
+++ FrontEnd/src/CreateAccount.js
@@ -18,6 +18,29 @@ export default function CreateAccount({setcreataccountprocess, setaccount}) {
     });
   };
 
+  const handleCreateAccount = async (event) => {
+    event.preventDefault();
+    console.log(formData);
+
+    fetch('http://127.0.0.1:5000/signup', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: formData,
+    })
+    .then(response => response.json())
+    .then(data => {
+        alert(data.message);
+        if (data.message) {
+            window.location.href = 'user_view.html'; // Redirect to sign-in page
+            setcreataccountprocess(true);
+            setaccount(true);
+        }
+    })
+    .catch(error => console.error('Error:', error));
+}
+
   return (
     <>
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
@@ -33,25 +56,9 @@ export default function CreateAccount({setcreataccountprocess, setaccount}) {
       </div>
 
       <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-        <form className="space-y-6" action="#" method="POST">
-        
-        <div class="mt-10 m:mx-auto sm:w-full sm:max-w-sm">
-      <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-state">
-        Are you wanting to sell a car?
-      </label>
-      <div class="relative">
-        <select class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
-          <option>No</option>
-          <option>Yes</option>
-        </select>
-        <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-          <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
-        </div>
-      </div>
-    </div>
-
+        <form className="space-y-6" action="#" method="POST" onSubmit={handleCreateAccount}>
         <div>
-            <label htmlFor="name" className="block text-sm font-medium leading-6 text-gray-900">
+            <label htmlFor="name" className="block text-sm font-medium leading-6 text-gray-900" onChange={handleChange}>
               Name
             </label>
             <div className="mt-2">
@@ -60,6 +67,7 @@ export default function CreateAccount({setcreataccountprocess, setaccount}) {
                 name="name"
                 type="name"
                 autoComplete="name"
+                onChange={handleChange}
                 required
                 className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
@@ -67,14 +75,14 @@ export default function CreateAccount({setcreataccountprocess, setaccount}) {
           </div>
 
           <div class="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-      <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-zip">
+      <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-zip" onChange={handleChange}>
         Age
       </label>
-      <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-zip" type="text" placeholder="18"/>
+      <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="age" name="age" type="age" placeholder="18" onChange={handleChange}/>
     </div>
 
           <div>
-            <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+            <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900" onChange={handleChange}>
               Email address
             </label>
             <div className="mt-2">
@@ -83,6 +91,7 @@ export default function CreateAccount({setcreataccountprocess, setaccount}) {
                 name="email"
                 type="email"
                 autoComplete="email"
+                onChange={handleChange}
                 required
                 className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
@@ -91,7 +100,7 @@ export default function CreateAccount({setcreataccountprocess, setaccount}) {
 
           <div>
             <div className="flex items-center justify-between">
-              <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+              <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900" onChange={handleChange}>
                 Password
               </label>
             </div>
@@ -101,6 +110,7 @@ export default function CreateAccount({setcreataccountprocess, setaccount}) {
                 name="password"
                 type="password"
                 autoComplete="current-password"
+                onChange={handleChange}
                 required
                 className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-black-600 sm:text-sm sm:leading-6"
               />
@@ -108,7 +118,7 @@ export default function CreateAccount({setcreataccountprocess, setaccount}) {
           </div>
           <div>
             <div className="flex items-center justify-between">
-              <label htmlFor="repassword" className="block text-sm font-medium leading-6 text-gray-900">
+              <label htmlFor="repassword" className="block text-sm font-medium leading-6 text-gray-900" onChange={handleChange}>
                 Re-Enter Password
               </label>
             </div>
@@ -118,6 +128,7 @@ export default function CreateAccount({setcreataccountprocess, setaccount}) {
                 name="repassword"
                 type="repassword"
                 autoComplete="current-password"
+                onChange={handleChange}
                 required
                 className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-black-600 sm:text-sm sm:leading-6"
               />
@@ -129,7 +140,7 @@ export default function CreateAccount({setcreataccountprocess, setaccount}) {
               type="submit"
               className="flex w-full justify-center rounded-md bg-blue-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
             >
-              Create
+              Create Account
             </button>
           </div>
         </form>
--- FrontEnd/src/Login.js
+++ FrontEnd/src/Login.js
@@ -1,22 +1,69 @@
-export default function Login({setloginprocess, setaccount}) {
-    return (
-      <>
-        <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+import React, { useState } from "react";
+
+export default function Login({ setloginprocess, setaccount }) {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (event) => {
+    setFormData({
+      ...formData,
+      [event.target.id]: event.target.value,
+    });
+  };
+
+  const handleLogin = async (event) => {
+    event.preventDefault();
+    console.log(formData.email);
+    console.log(formData.password);
+    fetch("http://127.0.0.1:5000/signin", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: formData,
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.message) {
+          localStorage.setItem("userRole", data.role); // Save user role
+          localStorage.setItem("userEmail", formData.email); // Save user email
+          window.location.href = "user_view.html"; // Redirect to user account page
+          setloginprocess(false);
+          setaccount(true);
+        } else {
+          alert(data.error);
+        }
+      })
+      .catch((error) => console.error("Error:", error));
+    setloginprocess(false);
+    setaccount(true);
+  };
+
+  return (
+    <>
+      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <img
-            className="mx-auto h-10 w-auto"
-            src=""
-            alt="Carnest"
-          />
+          <img className="mx-auto h-10 w-auto" src="" alt="Carnest" />
           <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
             Login to your account
           </h2>
         </div>
 
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-          <form className="space-y-6" action="#" method="POST">
+          <form
+            className="space-y-6"
+            action="#"
+            method="POST"
+            onSubmit={handleLogin}
+          >
             <div>
-              <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium leading-6 text-gray-900"
+                onChange={handleChange}
+              >
                 Email address
               </label>
               <div className="mt-2">
@@ -25,6 +72,7 @@ export default function Login({setloginprocess, setaccount}) {
                   name="email"
                   type="email"
                   autoComplete="email"
+                  onChange={handleChange}
                   required
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                 />
@@ -32,12 +80,22 @@ export default function Login({setloginprocess, setaccount}) {
             </div>
 
             <div>
-              <div className="flex items-center justify-between">
-                <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+              <div
+                className="flex items-center justify-between"
+                onChange={handleChange}
+              >
+                <label
+                  htmlFor="password"
+                  onChange={handleChange}
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
                   Password
                 </label>
                 <div className="text-sm">
-                  <a href="#" className="font-semibold text-blue-600 hover:text-indigo-500">
+                  <a
+                    href="#"
+                    className="font-semibold text-blue-600 hover:text-indigo-500"
+                  >
                     Forgot password?
                   </a>
                 </div>
@@ -48,6 +106,7 @@ export default function Login({setloginprocess, setaccount}) {
                   name="password"
                   type="password"
                   autoComplete="current-password"
+                  onChange={handleChange}
                   required
                   className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-black-600 sm:text-sm sm:leading-6"
                 />
@@ -66,6 +125,5 @@ export default function Login({setloginprocess, setaccount}) {
         </div>
       </div>
     </>
-
-    )
-  }
+  );
+}
