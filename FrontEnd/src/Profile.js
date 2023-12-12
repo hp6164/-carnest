e38f8641f1dc2e8567a48ca370b329364d6e7a2f
Will H <whuan126@ucr.edu>
2023-12-12 13:33:53 -0800

    Showing email contact

--- FrontEnd/src/Profile.js
+++ FrontEnd/src/Profile.js
@@ -4,8 +4,16 @@ import "./Profile.css";
 export default function Profile() {
     return (
         <div className ="profileContainer"> 
-            <span className="profileShowName"> Will H. </span>
-            <span className="profileShowEmail"> Sales Rep. </span>
+            <div className ="profileShow">
+                <div className = "profileTop">
+                    <span className ="profileShowName"> Will H. </span>
+                    <span className ="profileShowTitle"> Sales Rep. </span>
+                </div>
+                <div className ="profileBottom">
+                    <span className ="profileContentTitle"> Email </span>
+                    <span className ="profileInfo"> testing123@gmail.com </span>
+                </div>
+            </div>
         </div>
     );
 }
