a8c189561c921bacd727424a34a2602c2518e897
Will Huang <whuan126@ucr.edu>
2023-12-12 13:22:29 -0800

    Recommiting Profile.js

---
+++ FrontEnd/src/Profile.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Profile.css";
+
+export default function Profile() {
+    return (
+        <div className ="profileContainer"> 
+            <span className="profileShowName"> Will H. </span>
+            <span className="profileShowEmail"> Sales Rep. </span>
+        </div>
+    );
+}
