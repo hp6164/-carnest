0bbdf4f563c9b6fd686be34cac9025f58fe4e2f0
Will Huang <whuan126@ucr.edu>
2023-12-12 16:20:55 -0800

    Created data table of sellers per profile

--- FrontEnd/src/Profile.js
+++ FrontEnd/src/Profile.js
@@ -2,8 +2,38 @@ import {
     MailOutline,
     PhoneAndroid,
 } from "@material-ui/icons";
+import { DataGrid } from "@mui/x-data-grid";
 import React from "react";
 import "./Profile.css";
+
+const columns = [
+    { field: 'id', headerName: 'ID', width: 70 },
+    { field: 'firstName', headerName: 'First name', width: 130 },
+    { field: 'lastName', headerName: 'Last name', width: 130 },
+    {
+      field: 'phoneNumber',
+      headerName: 'Phone Number',
+      type: 'number',
+      width: 120,
+    },
+    {
+      field: 'carType',
+      headerName: 'Car',
+      width: 200,
+    },
+  ];
+
+const rows = [
+    { id: 1, lastName: 'Stark', firstName: 'Arya', phoneNumber: 714551234, carType: 'Honda Accord' },
+    { id: 2, lastName: 'Lannister', firstName: 'Tyrion', phoneNumber: 310794567, carType: 'Ford Fusion' },
+    { id: 3, lastName: 'Smith', firstName: 'John', phoneNumber: 562876234, carType: 'Chevrolet Malibu' },
+    { id: 4, lastName: 'Swell', firstName: 'Toby', phoneNumber: 909321568, carType: 'Nissan Altima' },
+    { id: 5, lastName: 'Grey', firstName: 'Alistar', phoneNumber: 415877890, carType: 'Mazda3' },
+    { id: 6, lastName: 'Martell', firstName: 'Kyle', phoneNumber: 650245678, carType: 'Subaru Impreza' },
+    { id: 7, lastName: 'Barrel', firstName: 'Robert', phoneNumber: 819876543, carType: 'Hyundai Elantra' },
+    { id: 8, lastName: 'Chu', firstName: 'Samuel', phoneNumber: 213865432, carType: 'Kia Forte' },
+    { id: 9, lastName: 'Arryn', firstName: 'Joel', phoneNumber: 323654321, carType: 'Volkswagen Jetta' },
+  ];
 
 export default function Profile() {
     return (
@@ -35,6 +65,19 @@ export default function Profile() {
                         </div>
                     </div>
                 </div>
+            </div>
+            <div style={{ height: 400, width: '100%' }}>
+                <DataGrid
+                    rows={rows}
+                    columns={columns}
+                    initialState={{
+                    pagination: {
+                        paginationModel: { page: 0, pageSize: 5 },
+                    },
+                    }}
+                    pageSizeOptions={[5, 10]}
+                    checkboxSelection
+                />
             </div>
         </div>
     );
--- FrontEnd/src/index.js
+++ FrontEnd/src/index.js
@@ -2,11 +2,12 @@ import React, { useState } from "react";
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from "./App";
+import Profile from "./Profile";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    {<App />}
+    {<Profile />}
   </React.StrictMode>
 );
 
